9642cf1a4c58a09905cdb24c4fa07e6dd1a3c0ff
unknown <you@example.comvinayprashant919@gmail.com>
2025-11-17 20:45:44 +0530

    added form for adding warehouses

--- neurofleetx/src/main/java/com/neurofleetx/neurofleetx/HomeController.java
+++ neurofleetx/src/main/java/com/neurofleetx/neurofleetx/HomeController.java
@@ -22,5 +22,13 @@ public class HomeController {
         model.addAttribute("steps", steps);
         return "result";
     }
+    @PostMapping("/addwarehouse")
+    public String addwarehouse(@RequestParam("name") String name, 
+                            @RequestParam("steps") int steps, 
+                            Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("steps", steps);
+        return "result";
+    }
 }
 
